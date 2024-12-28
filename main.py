69fa620199b67deac93b870ce4deb3a572a39359
ahmedgoodgame17x <ahmedxbox2023zz@gmail.com>
2024-12-28 12:27:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 1315692214957117563
+CHANNEL_ID = 1322470357742911489
 SELF_MUTE = True
 SELF_DEAF = False
 
